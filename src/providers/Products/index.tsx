9e460b54f7b15b10a5666055b6341b60bbd171df
guilherme <85708476+guilherme-colhado@users.noreply.github.com>
2022-09-07 19:01:55 -0300

    feat: product provider basico

--- src/providers/Products/index.tsx
+++ src/providers/Products/index.tsx
@@ -1,17 +1,44 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { dueMarketApi } from "../../services";
 
-interface ProductsProviderData {}
-
-interface ProductsProviderProps {
-  children: ReactNode;
+interface ProductsProviderData {
+  products: Products[]
 }
 
-export const ProductsContext = createContext<ProductsProviderData>(
-  {} as ProductsProviderData
-);
+interface ProductsProviderProps {
+  children: ReactNode
+}
+
+interface Products {
+  title: string
+  category: string
+  duedate: string
+  oldvalue: string
+  newvalue: string
+  image: string
+  userId: number
+  id: number
+}
+
+export const ProductsContext = createContext<ProductsProviderData>({} as ProductsProviderData)
 
 export const ProductsProvider = ({ children }: ProductsProviderProps) => {
-  return (
-    <ProductsContext.Provider value={{}}>{children}</ProductsContext.Provider>
-  );
+  const [products, setProducts] = useState<Products[]>([] as Products[])
+  const [productsCategory, setProductsCategory] = useState<Products[]>([] as Products[])
+  const [productsName, setProductsName] = useState<Products[]>([] as Products[])
+  const [productsMarket, setProductsMarket] = useState<Products[]>([] as Products[])
+
+  useEffect(()=>{
+    getProduct()
+  },[])
+
+  const getProduct = () => {
+    dueMarketApi.get('/products')
+    .then((res)=>setProducts(res.data))
+    .catch((error)=>console.log(error))
+  }
+
+  return <ProductsContext.Provider value={{products}}>
+      {children}
+    </ProductsContext.Provider>
 };
