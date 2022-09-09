1636104c116b37b31db515d7b91022a6fc71ec39
vanagila rodrigues <vanagilakedna@gmail.com>
2022-09-09 15:05:55 -0300

    feat: home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,20 @@
+import { HeaderComponent } from "../../components/Header";
+import { Box, Container } from "./style";
+
 export const HomePage = () => {
-  return <div>HOME</div>;
+  return (
+    <>
+      <HeaderComponent />
+
+      <Box>
+        <p>Lorem ipsum dolor sit amet</p>
+      </Box>
+
+      <Container>
+        <p>Use seu CEP para visualizar mercados e produtos perto de você</p>
+        <input type="number" placeholder="Seu CEP" />
+        <span>*Apenas números</span>
+      </Container>
+    </>
+  );
 };
