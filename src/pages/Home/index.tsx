df9a1916555d5351f9dd80f766b2e6dfd3acb836
Thiago Barcelos <thiagolbf@hotmail.com>
2022-09-09 21:46:58 -0300

    fix: return home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,1 +1,3 @@
-
+export const HomePage = () => {
+  return <div>HOME</div>;
+};
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,6 +1,7 @@
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useEffect, useState } from "react";
 import {
   Container,
   BackgroundContainer,
@@ -19,31 +20,85 @@ interface UserData {
   email: string;
   password: string;
   confirmPassword: string;
+  cpf: string;
+  tipo: string;
+  isCpf: boolean;
+  isCnpj: boolean;
+  submit: boolean;
 }
 
 export const RegisterPage = () => {
+  const [typeUser, setTypeUser] = useState("");
+  const [isCpf, setIsCpf] = useState(false);
+  const [isCnpj, setIsCnpj] = useState(false);
+
   const schema = yup.object().shape({
-    name: yup.string().required("Campo obrigatório"),
-    userType: yup.string().required("Campo obrigatório"),
-    img: yup.string().required("Campo obrigatório"),
-    document: yup.string().required("Campo obrigatório"),
-    CEP: yup.string().required("Campo obrigatório"),
-    email: yup.string().required("Campo obrigatório"),
-    password: yup.string().required("Campo obrigatório"),
-    passwordConfirm: yup
-      .string()
-      .oneOf([yup.ref("password")], "Senha diferentes")
-      .required("Campo obrigatório"),
+    name: yup.string().required("campo obrigatório"),
+    // userType: yup.string().required("Campo obrigatório"),
+    // img: yup.string().required("Campo obrigatório"),
+    // document: yup.string().required("Campo obrigatório"),
+    // CEP: yup.string().required("Campo obrigatório"),
+    tipo: yup.string().required("Campo obrigatório").min(2),
+    isCpf: yup.boolean(),
+    submit: yup.boolean(),
+    cpf: yup.string().when("submit", {
+      is: true,
+      then: yup.string().required("Campo obrigatório do CPF"),
+      otherwise: yup.string(),
+    }),
+    // email: yup.string().required("Campo obrigatório"),
+    // password: yup.string().required("Campo obrigatório"),
+    // confirmPassword: yup
+    //   .string()
+    //   .oneOf([yup.ref("password")], "Senha diferentes")
+    //   .required("Campo obrigatório"),
   });
+
+  // isEmail: Yup.boolean(),
+  //     email: Yup.string().when('isEmail', {
+  //        is: true,
+  //        then: Yup.string()
+  //        .required('Enter Email ID'),
+  //        otherwise: Yup.string(),
+  //     }),
+
+  //   onst YOUR_SCHEMA_NAME = yup.object().shape({
+  //     email: yup.string().when('email', {
+  //         is: (exists) => !!exists,
+  //         then: yup.string().email(),
+  //         otherwise: yup.string(),
+  //     }),
+  // }, [['email', 'email']]);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<UserData>({ resolver: yupResolver(schema) });
+  } = useForm<UserData>({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
 
-  const registerSubmit = (userdata: UserData) => {};
+  const change = (type: string) => {
+    if (type === "cpf") {
+      setIsCnpj(false);
+      setIsCpf(true);
+    } else if (typeUser === "cnpj") {
+      setIsCpf(false);
+      setIsCnpj(true);
+    } else {
+      setIsCnpj(false);
+      setIsCpf(false);
+    }
+  };
+  useEffect(() => {
+    change(typeUser);
+    console.log(typeUser);
+  }, [typeUser]);
 
+  const registerSubmit = (userdata: UserData) => {
+    console.log(userdata);
+  };
   return (
     <Container>
       <BackgroundContainer>
@@ -60,22 +115,57 @@ export const RegisterPage = () => {
           <h2>Cadastro</h2>
           <Form onSubmit={handleSubmit(registerSubmit)}>
             <input type="text" {...register("name")} placeholder="teste" />
-            <input type="text" {...register("userType")} placeholder="teste" />
+            <span>{errors.name?.message}</span>
+            {/* <input type="text" {...register("userType")} placeholder="teste" />
+            {errors.userType?.message}
             <input type="text" {...register("img")} placeholder="teste" />
+            {errors.img?.message}
             <input type="text" {...register("document")} placeholder="teste" />
+            {errors.document?.message}
             <input type="text" {...register("CEP")} placeholder="teste" />
+            {errors.CEP?.message}
             <input type="text" {...register("email")} placeholder="teste" />
-            <input
+            {errors.email?.message} */}
+            <select
+              {...register("tipo")}
+              onChange={(e) => setTypeUser(e.target.value)}
+            >
+              <option value="">Formato de usuário</option>
+              <option value="cpf">Usuário</option>
+              <option value="cnpj">Cliente</option>
+            </select>
+            {typeUser === "cpf" ? (
+              <>
+                <input
+                  placeholder="Digite seu CPF"
+                  type="text"
+                  {...register("cpf")}
+                />
+                {errors.cpf?.message}
+              </>
+            ) : typeUser === "cnpj" ? (
+              <>
+                <input placeholder="Digite seu CNPJ" />
+                <input placeholder="Coloque link de sua logomarca" />
+              </>
+            ) : null}
+
+            {/* <input
               type="password"
               {...register("password")}
               placeholder="teste"
             />
+            {errors.password?.message}
             <input
               type="password"
               {...register("confirmPassword")}
               placeholder="teste"
             />
-            <button>Cadastrar</button>
+            {errors.confirmPassword?.message} */}
+            {errors.tipo?.message}
+            <input type="checkbox" {...register("submit")} />
+
+            <button type="submit">Cadastrar</button>
           </Form>
         </FormContainer>
         <GreenBox />
