42ba02fae8e9f312965e0c3c0198ce9fd28cbc75
Thiago Barcelos <thiagolbf@hotmail.com>
2022-09-13 12:55:34 -0300

    feat: add whishlit provider

--- src/providers/Wishlist/index.tsx
+++ src/providers/Wishlist/index.tsx
@@ -1,9 +1,27 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { dueMarketApi } from "../../services/index";
+import { toast } from "react-toastify";
 
-interface WhishListProviderData {}
+interface WhishListProviderData {
+  whishlist: WhishList[];
+  getWhishListByUser: (userId: number, token: string, data: WhishList) => void;
+  addProductWhishList: (userId: number, token: string, data: WhishList) => void;
+  deletProductWhishlist: (id: number, token: string) => void;
+}
 
 interface WishListProviderProps {
   children: ReactNode;
+}
+
+interface WhishList {
+  title: string;
+  category: string;
+  duedate: string;
+  oldvalue: string;
+  newvalue: string;
+  image: string;
+  userId: number;
+  id: number;
 }
 
 export const WhishListContext = createContext<WhishListProviderData>(
@@ -11,7 +29,62 @@ export const WhishListContext = createContext<WhishListProviderData>(
 );
 
 export const WhishListProvider = ({ children }: WishListProviderProps) => {
+  const [whishlist, setWishList] = useState<WhishList[]>([] as WhishList[]);
+
+  const getWhishListByUser = (userId: number, token: string) => {
+    dueMarketApi
+      .get(`/whishlist?userId=${userId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => setWishList(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  const addProductWhishList = (
+    userId: number,
+    token: string,
+    data: WhishList
+  ) => {
+    const whishlist = { ...data, userId };
+    dueMarketApi
+      .post(`/whishlist`, whishlist, {
+        headers: { Authorization: `Beares ${token}` },
+      })
+      .then((res) => {
+        toast.success("Adicionado produto a lista de desejos");
+        setWishList(res.data);
+      })
+      .catch((err) => {
+        toast.error("Não foi possível adicionar produto a lista de desejos");
+        console.log(err);
+      });
+  };
+
+  const deletProductWhishlist = (id: number, token: string) => {
+    dueMarketApi
+      .delete(`/whishlist/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        setWishList(res.data);
+        toast.success("Produto deletado da lista de desejos");
+      })
+      .catch((err) => {
+        toast.error("Não foi possível deletar o produto da lista de desejos");
+        console.log(err);
+      });
+  };
+
   return (
-    <WhishListContext.Provider value={{}}>{children}</WhishListContext.Provider>
+    <WhishListContext.Provider
+      value={{
+        whishlist,
+        getWhishListByUser,
+        addProductWhishList,
+        deletProductWhishlist,
+      }}
+    >
+      {children}
+    </WhishListContext.Provider>
   );
 };
