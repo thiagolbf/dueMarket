a9011f9a839f7e8b78e5f51847040a7f1e706156
guilherme <85708476+guilherme-colhado@users.noreply.github.com>
2022-09-15 20:21:53 -0300

    feat: add wishlist user page not functional

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -7,28 +7,38 @@ import { useContext, useEffect, useState } from "react"
 import { UsersContext } from "../../providers/Users"
 import { useNavigate } from "react-router-dom"
 import { ProductsContext } from "../../providers/Products"
-import { title } from "process"
+import { WhishListContext } from "../../providers/Wishlist"
+import { ModalCupom } from "../../components/ModalCupom/style"
+import { ModalCupomComponent } from "../../components/ModalCupom"
+import { ModalCriarProduto } from "../../components/ModalCriarProduto"
 
 export const UserPage = () => {
   const [activeRD, setActiveRD] = useState(false)
+  const [modalCupom, setModalCupom] = useState(false)
+  const [modalProduct, setModalProduct] = useState(false)
   const { user } = useContext(UsersContext)
   const { products, getProductByMarket } = useContext(ProductsContext)
+  const { whishlist, getWhishListByUser } = useContext(WhishListContext)
   const nav = useNavigate()
   if(!localStorage.getItem('@dueMarket:token')){
     nav('/login')
   }
   useEffect(()=>{
-    getProductByMarket(Number(localStorage.getItem('@dueMarket:userId')))
-  },[])
-  console.log(products)
+    if(user.type==="mercado"){
+      getProductByMarket(Number(localStorage.getItem('@dueMarket:userId')))
+    }else if(user.type==="cliente"){
+      getWhishListByUser(Number(localStorage.getItem('@dueMarket:userId')), String(localStorage.getItem("@dueMarket:token")))
+    }
+  },[user])
+  console.log(user)
   return <>
     <HeaderComponent/>
     <UserMain>
       <div>
         <p>Lorem ipsum dolor sit amet.</p>
         <div>
-          <button>Cupom</button>
-          <button>Cadastrar produto</button>
+          <button onClick={()=>setModalCupom(true)}>Cupom</button>
+          <button onClick={()=>setModalProduct(true)}>Cadastrar produto</button>
           <button onClick={()=>setActiveRD(true)}>
             <FaPencilAlt/>
           </button>
@@ -38,23 +48,34 @@ export const UserPage = () => {
       <div>
           <h2>
             {
-            user.type === undefined ?
+            user === undefined ?
             '' 
             :
-            user.type === "user" ?
+            user.type === "cliente" ?
             "Lista de desejos" 
             :
             "Produtos Cadastrados"
             }
           </h2>
         <div>
-        {
+          {
             user.type === undefined ?
             '' 
             :
             user.type === "mercado" ?
             products.map(product => <CardProductComponent
-              user={user}
+              userType="mercado"
+              date={product.duedate} 
+              title={product.title} 
+              img={product.image} 
+              newValue={product.newvalue}
+              previusValue={product.oldvalue}
+              type={product.category}  
+              wishlist={true}
+            />)
+            :
+            whishlist.map((product) => <CardProductComponent
+              userType="cliente"
               date={product.duedate} 
               title={product.title} 
               img={product.image} 
@@ -62,12 +83,12 @@ export const UserPage = () => {
               previusValue={product.oldvalue}
               type={product.category}  
             />)
-            :
-            "Produtos Cadastrados"
-            }
+          }
         </div>
         <RegisterDataComponent setActiveRD={setActiveRD} activeRD={activeRD}/>
       </div>
     </UserMain>
+    <ModalCupomComponent modalCupom={modalCupom} setModalCupom={setModalCupom}></ModalCupomComponent>
+    <ModalCriarProduto modalProduto={modalProduct} setModalProduto={setModalProduct}></ModalCriarProduto>
   </>
 }
