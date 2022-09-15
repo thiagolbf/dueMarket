48c8999fb5c2d3f583030cd79e83195aef12a14f
Thiago Barcelos <thiagolbf@hotmail.com>
2022-09-14 22:35:38 -0300

    feat/ConfirmationModal

---
+++ src/components/ModalConfirmation/index.tsx
@@ -0,0 +1,46 @@
+import { ModalConfirmation } from "./style";
+import { Dispatch, SetStateAction } from "react";
+import { HeaderModalComponent } from "../HeaderModal";
+import { Button } from "../Button";
+interface ModalConfirmationComponentProps {
+  modalConfirmation: boolean;
+  setModalConfirmation: Dispatch<SetStateAction<boolean>>;
+}
+
+interface TargetProps extends EventTarget {
+  id: string;
+}
+
+// TESTAR COM O BOTÃO DE EXCLUIR PRODUTOS - TANTO PARA MERCADO TANTO PARA WISHLIST DO USUÁRIO
+
+export const ModalConfirmationComponent = ({
+  modalConfirmation,
+  setModalConfirmation,
+}: ModalConfirmationComponentProps) => {
+  const handleEvemt = (id: string) => {
+    if (id === "modalConfirmation") {
+      setModalConfirmation(false);
+    }
+  };
+
+  return (
+    <ModalConfirmation
+      id="modalConfirmation"
+      onClick={(e) => handleEvemt((e.target as TargetProps).id)}
+    >
+      <div>
+        <HeaderModalComponent setState={setModalConfirmation}>
+          Deseja concluir a ação?
+        </HeaderModalComponent>
+        <div className="modalConfirmationButtons">
+          <Button lightGreyForm onClick={() => console.log("SIM")}>
+            Sim
+          </Button>
+          <Button blueForm onClick={() => setModalConfirmation(false)}>
+            Não
+          </Button>
+        </div>
+      </div>
+    </ModalConfirmation>
+  );
+};
